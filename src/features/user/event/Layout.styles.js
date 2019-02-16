aa3cec5b5ccaba4bc0fc8c8a94c8d44fd5d0dbe3
Patrick Garcia <pkmgarcia@gmail.com>
2019-02-16 13:18:25 -0800

    Removed unecessary class

--- src/features/user/event/Layout.styles.js
+++ src/features/user/event/Layout.styles.js
@@ -10,12 +10,6 @@ const styles = {
     justifyContent: 'flex-end',
     paddingBottom: '48px',
   },
-  dialogPaper: {
-    minWidth: 'fit-content',
-    maxWidth: '600px',
-    minHeight: '66vh',
-    maxHeight: '66vh',
-  },
   fab: {
     position: 'absolute',
     bottom: '32px',
